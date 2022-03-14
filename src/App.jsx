a51ea6ef4cde5138467fe56792aee00afe29bba0
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-03-14 18:45:29 -0300

    creadas rutas principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import IniciarSesion from './layout/IniciarSesion'
+import Layout from './layout/Layout';
+import Inicio from './paginas/Inicio';
+import NuevoCliente from './paginas/NuevoCliente';
+import EditarCliente from './paginas/EditarCliente';
 
 
 function App() {
@@ -9,8 +13,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
-          <Route path="/" element={<IniciarSesion />} />
+        <Route path="/clientes" element={<Layout />}>
+          <Route index element={<Inicio />} />
+          <Route path="nuevo" element={<NuevoCliente />} />
+          <Route path="editar/:id" element={<EditarCliente />} />
         </Route>
 
       </Routes>
