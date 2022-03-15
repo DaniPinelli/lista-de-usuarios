34a16f67807c29ba5cf0f4ad929ec0cc9abe6c2c
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-03-15 17:07:13 -0300

    estilos y componente formulario

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Formulario = () => {
+    return (
+        <div className="bg-slate-300 mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto" >
+            <h2 className="text-blue-900 font-bold text-xl uppercase text-center" >Agregar Cliente</h2>
+        </div>
+    )
+}
+
+export default Formulario
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,49 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 
 const Layout = () => {
-    return (
-        <div><p>Layout</p>
 
-            <Outlet />
+    const location = useLocation()
+    const urlActual = location.pathname
+
+    console.log(location)
+
+    return (
+        <div className="md:flex md:min-h-screen bg-slate-500">
+
+            <div className="md:w-1/4 bg-blue-900 px-5 py-10 ">
+                <h2 className="text-white text-4xl font-black text-center"
+                >CRM - Clientes</h2>
+                <nav className="mt-10" >
+                    <Link
+                        to="/clientes" className={`${urlActual === '/clientes' ? 'text-purple-300' : 'text-white'} block mt-2 text-2xl hover:text-blue-200`}>
+                        Inicio
+                    </Link>
+                    <Link
+                        to="/clientes/nuevo" className={`${urlActual === '/clientes/nuevo' ? 'text-purple-300' : 'text-white'} block mt-2 text-2xl hover:text-blue-200`}>
+                        Nuevo Cliente
+                    </Link>
+                    <Link
+                        to="/clientes/editar/:id" className={`${urlActual === '/clientes/editar/:id' ? 'text-purple-300' : 'text-white'} block mt-2 text-2xl hover:text-blue-200`}>
+                        Editar Cliente
+                    </Link>
+
+                </nav>
+            </div>
+            <div className="md:w-3/4 p-10">
+                <Outlet />
+            </div>
         </div>
     )
 }
 
 export default Layout
+
+
+
+
+
+
+
+
+
+
